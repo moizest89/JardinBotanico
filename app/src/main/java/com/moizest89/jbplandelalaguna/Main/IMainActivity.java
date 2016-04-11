8da42adf05dac98dc25f7adbc254d17bbe0376b2
moizest89 <moises.gilberto@gmail.com>
2016-04-10 19:16:14 -0600

    menu drawer

---
+++ app/src/main/java/com/moizest89/jbplandelalaguna/Main/IMainActivity.java
@@ -0,0 +1,7 @@
+package com.moizest89.jbplandelalaguna.Main;
+
+/**
+ * Created by @moizest89 in SV on 4/10/16.
+ */
+public interface IMainActivity {
+}
--- app/src/main/java/com/moizest89/jbplandelalaguna/Main/MainActivity.java
+++ app/src/main/java/com/moizest89/jbplandelalaguna/Main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moizest89.jbplandelalaguna;
+package com.moizest89.jbplandelalaguna.Main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.moizest89.jbplandelalaguna.R;
 
 public class MainActivity extends AppCompatActivity {
 
