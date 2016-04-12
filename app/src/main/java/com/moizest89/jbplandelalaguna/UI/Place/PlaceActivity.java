61b49305ea9b9692b2adf33392aa4622bba1e71e
moizest89 <moises.gilberto@gmail.com>
2016-04-11 22:51:38 -0600

    spinner headers and labels, ImageTouch and set Place image

--- app/src/main/java/com/moizest89/jbplandelalaguna/UI/Place/PlaceActivity.java
+++ app/src/main/java/com/moizest89/jbplandelalaguna/UI/Place/PlaceActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import com.moizest89.jbplandelalaguna.R;
@@ -15,13 +17,17 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class PlaceActivity extends AppCompatActivity implements IPlaceActivity{
+public class PlaceActivity extends AppCompatActivity implements IPlaceActivity,
+        Spinner.OnItemSelectedListener{
 
     @Bind(R.id.toolbar_spinner)
     Spinner toolbar_spinner;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
 
     private PlacePresenter mPresenter;
+    private final static String TAG = PlaceActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +36,28 @@ public class PlaceActivity extends AppCompatActivity implements IPlaceActivity{
 
         ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setToolbar();
 
         this.mPresenter = new PlacePresenter(this);
         this.mPresenter.loadDataForSpinner();
 
+    }
+
+    private void setToolbar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_short_name));
+        getSupportActionBar().setSubtitle(getResources().getString(R.string.place_title));
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -45,7 +66,24 @@ public class PlaceActivity extends AppCompatActivity implements IPlaceActivity{
         ZoneSpinnerAdapter zoneSpinnerAdapter = new ZoneSpinnerAdapter(this, mData);
         this.toolbar_spinner.setAdapter(zoneSpinnerAdapter);
         zoneSpinnerAdapter.notifyDataSetChanged();
+        this.toolbar_spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(position != 0) {
+            Log.e(TAG, "onItemSelected");
+            Log.e(TAG, "position: " + position);
+            Log.e(TAG, "long: " + id);
+        }
+
+        parent.setSelection(0);
 
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Log.e(TAG, "onNothingSelected");
+        Log.e(TAG, "parent.getFirstVisiblePosition(): "+parent.getFirstVisiblePosition());
+    }
 }
--- app/src/main/java/com/moizest89/jbplandelalaguna/UI/Place/PlacePresenter.java
+++ app/src/main/java/com/moizest89/jbplandelalaguna/UI/Place/PlacePresenter.java
@@ -1,6 +1,8 @@
 package com.moizest89.jbplandelalaguna.UI.Place;
 
 
+import com.moizest89.jbplandelalaguna.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +12,25 @@ import java.util.List;
 public class PlacePresenter {
 
     private PlaceActivity context;
+    private String mSpinnerHeader;
+    private String mSpinnerZoneLabel;
 
     public PlacePresenter(PlaceActivity context) {
         this.context = context;
+        this.mSpinnerHeader = this.context.getResources().getString(R.string.place_spinner_label_select_item);
+        this.mSpinnerZoneLabel = this.context.getResources().getString(R.string.place_spinner_label_zone);
     }
 
 
     public void loadDataForSpinner(){
 
         List<String> mData = new ArrayList<>();
-        for (int i = 1; i < 32; i++) {
-            mData.add("Zona "+i);
+        for (int i = 0; i < 32; i++) {
+            if (i == 0) {
+                mData.add(this.mSpinnerHeader);
+            } else{
+                mData.add(this.mSpinnerZoneLabel+" "+ i);
+            }
         }
 
         this.context.setDataInSpinner(mData);
