82f9aa3465ad927425311272916df8b0f4e45b9b
moizest89 <moises.gilberto@gmail.com>
2016-05-05 01:43:24 -0600

    code bar QR

--- app/src/main/java/com/moizest89/jbplandelalaguna/UI/BarcodeView/BarcodeActivity.java
+++ app/src/main/java/com/moizest89/jbplandelalaguna/UI/BarcodeView/BarcodeActivity.java
@@ -1,22 +1,22 @@
 package com.moizest89.jbplandelalaguna.UI.BarcodeView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
-import android.view.View;
+import android.util.Log;
 
-import com.journeyapps.barcodescanner.CaptureManager;
-import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
 import com.moizest89.jbplandelalaguna.R;
 
-public class BarcodeActivity extends AppCompatActivity implements CompoundBarcodeView.TorchListener{
+import me.sudar.zxingorient.Barcode;
+import me.sudar.zxingorient.ZxingOrient;
+import me.sudar.zxingorient.ZxingOrientResult;
+
+public class BarcodeActivity extends AppCompatActivity{
 
 
-    private CompoundBarcodeView compoundBarcodeView;
-    private CaptureManager capture;
+    private final static String TAG =BarcodeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,52 +25,71 @@ public class BarcodeActivity extends AppCompatActivity implements CompoundBarcod
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        this.compoundBarcodeView = (CompoundBarcodeView) this.findViewById(R.id.zxing_barcode_scanner);
-        this.compoundBarcodeView.setTorchListener(this);
 
-        capture = new CaptureManager(this, this.compoundBarcodeView);
-        capture.initializeFromIntent(getIntent(), savedInstanceState);
-        capture.decode();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        capture.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        capture.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        capture.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        capture.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return this.compoundBarcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+        scanNow();
     }
 
 
-    @Override
-    public void onTorchOn() {
+    public void scanNow(){
+//        IntentIntegrator integrator = new IntentIntegrator(this);
+//        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+//        integrator.setPrompt("Escanea el codigo QR");
+//        integrator.setCameraId(0);  // Use a specific camera of the device
+//        integrator.setBeepEnabled(true);
+//        integrator.setBarcodeImageEnabled(false);
+//        integrator.setOrientationLocked(false);
+//
+//        integrator.initiateScan();
+
+        new ZxingOrient(BarcodeActivity.this)
+                .showInfoBox(false) // Doesn't display the info box
+                .setBeep(false)  // Doesn't play beep sound
+                .setVibration(true)  // Enables the vibration
+                .setIcon(R.drawable.ic_launcher)
+                .setToolbarColor("#AA00796B")
+                .setInfoBoxColor("#AA00796B")
+                .setInfo("Escanea el codigo QR")
+                .initiateScan(Barcode.QR_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+//
+//        Log.e(TAG, "requestCode: "+requestCode);
+//        Log.e(TAG, "resultCode: "+resultCode);
+//
+//        Log.e(TAG, "RESULT_CANCELED: "+RESULT_CANCELED);
+//        Log.e(TAG, "RESULT_OK: "+RESULT_OK);
+//
+//        Log.e(TAG, "scanningResult: "+scanningResult);
+
+        if(resultCode == RESULT_OK) {
+
+//            if (scanningResult != null) {
+//                String scanContent = scanningResult.getContents();
+//                String scanFormat = scanningResult.getFormatName();
+//
+//                Log.e(TAG, "FORMAT: " + scanFormat);
+//                Log.e(TAG, "CONTENT: " + scanContent);
+//
+//
+//            } else {
+//                Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+            ZxingOrientResult scanResult = ZxingOrient.parseActivityResult(requestCode, resultCode, intent);
+
+            if (scanResult != null) {
+                String scanContent = scanResult.getContents();
+                String scanFormat = scanResult.getFormatName();
+
+                Log.e(TAG, "FORMAT: " + scanFormat);
+                Log.e(TAG, "CONTENT: " + scanContent);
+            }
+        }else{
+            finish();
+        }
 
     }
 
-    @Override
-    public void onTorchOff() {
-
-    }
 }
