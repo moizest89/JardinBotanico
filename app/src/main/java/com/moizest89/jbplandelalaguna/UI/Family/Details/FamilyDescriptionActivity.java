f63828a94cde16e39d3b6fdf9355a5c00fdbe4d0
moizest89 <moises.gilberto@gmail.com>
2016-05-14 15:38:08 -0600

    family gallery images

--- app/src/main/java/com/moizest89/jbplandelalaguna/UI/Family/Details/FamilyDescriptionActivity.java
+++ app/src/main/java/com/moizest89/jbplandelalaguna/UI/Family/Details/FamilyDescriptionActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
@@ -19,13 +21,20 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.moizest89.jbplandelalaguna.Data.models.Family;
 import com.moizest89.jbplandelalaguna.Data.models.FamilyImage;
+import com.moizest89.jbplandelalaguna.Data.models.TipImage;
 import com.moizest89.jbplandelalaguna.R;
+import com.moizest89.jbplandelalaguna.UI.Tips.Details.TipsListDetailsActivity;
+import com.moizest89.jbplandelalaguna.UI.Tips.Details.TipsListDetailsImagesAdapter;
+import com.moizest89.jbplandelalaguna.UI.Tips.Gallery.TipGalleryImagesActivity;
 import com.moizest89.jbplandelalaguna.Util.Fonts;
+import com.moizest89.jbplandelalaguna.Util.MarginDecoration;
+import com.moizest89.jbplandelalaguna.Util.OnItemClickListener;
 import com.moizest89.jbplandelalaguna.Util.Util;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -35,6 +44,10 @@ public class FamilyDescriptionActivity extends AppCompatActivity implements IFam
 
     @Bind(R.id.TVTDescription)
     TextView TVTDescription;
+    @Bind(R.id.TVLabel)
+    TextView TVLabel;
+
+
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
@@ -44,6 +57,9 @@ public class FamilyDescriptionActivity extends AppCompatActivity implements IFam
 
     @Bind(R.id.collapsing_toolbar)
     CollapsingToolbarLayout collapsing_toolbar;
+
+    @Bind(R.id.RVList)
+    RecyclerView RVList;
 
 
 
@@ -66,6 +82,7 @@ public class FamilyDescriptionActivity extends AppCompatActivity implements IFam
         setToolbar();
         setDialog();
         this.TVTDescription.setTypeface(new Fonts().RobotoCondensed_Regular(this));
+        this.TVLabel.setTypeface(new Fonts().RobotoCondensed_Regular(this));
 
         Intent intent = getIntent();
         if(intent != null){
@@ -137,6 +154,7 @@ public class FamilyDescriptionActivity extends AppCompatActivity implements IFam
 
             List<FamilyImage> images = family.getFamilyImages();
 
+            setImages(images);
             if(images.size() > 0) {
                 //Banner
                 FamilyImage first_image = images.get(0);
@@ -153,5 +171,38 @@ public class FamilyDescriptionActivity extends AppCompatActivity implements IFam
             }
 
         }
+    }
+
+    private void setImages(List<FamilyImage> data){
+        this.RVList.addItemDecoration(new MarginDecoration(5, this));
+        this.RVList.setHasFixedSize(true);
+        final GridLayoutManager manager = new GridLayoutManager(this,3);
+        this.RVList.setLayoutManager(manager);
+
+        final List<String> mImages = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            FamilyImage familyImage = data.get(i);
+            mImages.add(familyImage.getUrl());
+        }
+
+        FamilyDescriptionImagesAdapter adapter = new FamilyDescriptionImagesAdapter(this, data);
+        this.RVList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
+
+
+        OnItemClickListener onItemClickListener = new OnItemClickListener() {
+            @Override
+            public void onItemClickListener(View view, Integer position) {
+
+                Intent intent = new Intent(FamilyDescriptionActivity.this, TipGalleryImagesActivity.class);
+                intent.putStringArrayListExtra(Util.INTENT_DATA_SEND,(ArrayList<String>) mImages);
+                intent.putExtra(Util.INTENT_DATA_CARROUSEL_POSITION, position);
+                startActivity(intent);
+            }
+        };
+
+        adapter.setOnItemClickListener(onItemClickListener);
     }
 }
